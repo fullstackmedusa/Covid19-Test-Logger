b892ca1e88ce2c0a689dcf2439ff4cf958a5ad3c
Alondra Maldonado <alondra.mal96@gmail.com>
2022-01-13 13:59:16 -0800

    almost at mvp. Delete function redirection issue and _id issue needs fixing

--- controllers/tests.js
+++ controllers/tests.js
@@ -1,39 +1,61 @@
-const {Profile, Test} = require('../models/profile');
+const { Profile, Test } = require('../models/profile');
 
 module.exports = {
     create,
     new: newTest,
     show,
-  };
+    delete: deleteTest,
+};
 
-  function create(req, res){
+function create(req, res) {
 
-	// What can I log to see the contents of the form?
-	// req.body is the contents of the form
-	// what the user filled out
-	console.log(req.body, " <- req.body in the create function in my testsCtrl")
+    // What can I log to see the contents of the form?
+    // req.body is the contents of the form
+    // what the user filled out
+    console.log(req.body, " <- req.body in the create function in my testsCtrl")
 
-    Test.create({test_result:req.body.test_result, test_type: req.body.test_type, user_id:req.user._id}, function(err, test) {
+    Test.create({ test_result: req.body.test_result, test_type: req.body.test_type, user_id: req.user._id, date_tested: req.body.date_tested}, function (err, test) {
         test.save();
         res.redirect("/profile");
-	})
+    })
 };
 
 
-  function newTest(req, res) {
-        res.render("tests/new", {
-            title: "New Test",
-        }
-        )};
+function newTest(req, res) {
+    const newTest = new Test();
+    const dt = newTest.date_tested;
+    let timezoneOffset = dt.getTimezoneOffset() * 60000;
+    // subtract offset from the default date
+    const testDate = new Date(dt - timezoneOffset).toISOString();
+    res.render("tests/new", {
+        title: "New Test",
+        testDate,
+    }
+    )
+};
 
-        function show(req, res) {
-              Test.findById(req.params.id , function (err, test) {
-                res.render('tests/show', {
-                  test,
-                  title: "test details"
-                });
-              });
-            };
+function show(req, res) {
+    Test.findById(req.params.id, function (err, test) {
+        res.render('tests/show', {
+            test,
+            title: "test details"
+        });
+    });
+};
+
+
+
+
+function deleteTest(req, res, next) {
+    Profile.findById(req.user._id, function (err, profileDoc) {
+
+        profileDoc.test.pull({ user_id:profileDoc._id});
         
-          
+        console.log(profileDoc.tests, `<-- test we wanna delete`);
 
+        profileDoc.save(function (err) {
+            res.redirect("/show"); 
+        });
+
+    });
+}
--- routes/tests.js
+++ routes/tests.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const testsCtrl = require("../controllers/tests");
 
+router.delete("/profile/tests/:id",testsCtrl.delete);
 router.post("/", testsCtrl.create);
 
 router.get("/:id/show", testsCtrl.show);
